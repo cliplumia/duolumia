293cb430d38b45bc0b0ac520f78e001430821096
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-18 00:28:02 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,6 @@
   import { page } from '$app/stores';
   import '../app.css';
 
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-  $: if (browser && analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
 </script>
 
 <Header />
