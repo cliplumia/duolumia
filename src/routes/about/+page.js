19304ee89c55fb2fca3591c5152a997727dbd87a
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-18 21:13:15 +0200

    Update +page.js

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,8 +1,8 @@
-import { browser, dev } from '$app/env';
+import { dev } from '$app/environment';
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement...
-export const hydrate = dev;
+export const csr = dev;
 
 // ...but if the client-side router is already loaded
 // (i.e. we came here from elsewhere in the app), use it
