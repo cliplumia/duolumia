fd8e0d3a84a929b087975fb2eb853c8d827f64b6
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-19 15:32:20 +0200

    Update svelte.config.js
    fix : use adapter - netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
-    })
-  }
+	kit: {
+		adapter: adapter()
+	}
 };
 
-export default config; 
+export default config;
