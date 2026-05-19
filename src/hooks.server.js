fad68a84a297bd5de08e33f0cc9966cd4fd6474f
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-19 15:19:19 +0200

    Update hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,7 @@
 export const handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	if (event.url.pathname === '/favicon.png') {
-		return new Response(null, { status: 200 });
+		return new Response(null, { status: 404 });
 	}
 	return response;
 };
