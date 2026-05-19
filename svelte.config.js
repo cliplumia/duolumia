1089e662c783d24df77be3cfb2cd292ed39bbf6c
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-19 16:25:23 +0200

    Update svelte.config.js
    ignore missing favicon.png during prerender to prevent build fail

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import adapter from '@sveltejs/adapter-netlify';
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: 'warn'
+		}
 	}
 };
-
 export default config;
