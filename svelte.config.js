6a51b64879d9f59ff723a4fc2dfcd533126b61a9
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-25 19:42:19 +0200

    Change adapter from Netlify to Cloudflare
    feat: build changement pour Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
-const config = {
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			handleHttpError: 'warn'
-		}
-	}
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
-export default config;
