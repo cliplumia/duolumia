95e3c8e6380a6fb0bc09f0c7aa3478e6aa19421e
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-31 16:47:58 +0200

    Update +page.server.ts

--- src/routes/inscription/+page.server.ts
+++ src/routes/inscription/+page.server.ts
@@ -6,7 +6,7 @@ export const actions: Actions = {
     const email = data.get('email');
     const nom = data.get('nom');
 
-    const bd = platform.env.BD; // BD = le nom que t'as mis dans Cloudflare
+    const bd = platform.env.BD; 
 
     await bd.prepare(
       'INSERT INTO clients (nom, email) VALUES (?, ?)'
