f94562e3ba888c5db8de8db84553e70f818b3ab4
cliplumia <lya.emeraude.officiel@gmail.com>
2026-05-31 17:57:17 +0200

    Update app.d.ts
    fix: pour corriger erreur

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,19 @@
-declare  global {
+/// <reference types="@sveltejs/kit" />
+
+declare global {
   namespace App {
     interface Platform {
       env: {
-        BD: D1database
-      }
+        BD: D1Database;
+        GOOGLE_CLIENT_ID?: string;
+        GOOGLE_CLIENT_SECRET?: string;
+        OPENAI_API_KEY?: string;
+        API_SECRET?: string;
+      };
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      };
+      caches: CacheStorage & { default: Cache };
     }
   }
 }
